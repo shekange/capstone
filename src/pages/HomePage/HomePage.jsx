354798738fa9176175bfc1309ed0c0486c482119
Angel <angeliqueshek@gmail.com>
2024-12-01 22:48:22 -0500

    Clean up unused files

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./HomePage.scss";
 
 export default function HomePage() {
   const [timeLeft, setTimeLeft] = useState({
