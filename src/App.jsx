23e9676769cc1e2ce811eba25d9549adafbdbb31
Angel <angeliqueshek@gmail.com>
2024-12-03 23:51:48 -0500

    Add Q and A in the QA page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import BorderPage from "./pages/BorderPage/BorderPage.jsx";
 import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx"
+import Footer from "./components/Footer/Footer.jsx";
+import QA from "./pages/QAPage/QA.jsx"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/decorate" element={<BorderPage />} />
+            <Route path="/q-a" element={<QA />} />
           </Routes>
           <Footer />
         </BrowserRouter>
