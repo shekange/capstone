3f1de0637996ed710071988c0c1efe6b8a9f31c3
Angel <angeliqueshek@gmail.com>
2024-12-05 23:45:54 -0500

    Adjust the homepage image size

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,13 @@ import "./HomePage.scss";
 export default function HomePage() {
   return (
     <>
-      <img
-        className="homepage__image"
-        src="/src/assets/images/homepage-photo.JPG"
-        alt="HomePage Proposal Image"
-      ></img>
+      <div className="homepage">
+        <img
+          className="homepage__image"
+          src="/src/assets/images/homepage-photo.JPG"
+          alt="HomePage Proposal Image"
+        ></img>
+      </div>
     </>
   );
 }
